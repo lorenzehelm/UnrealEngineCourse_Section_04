997afc47a2e6789132b685d9e68d96068a0d7c23
v_miturev <v_miturev@wargaming.net>
2018-11-27 17:01:24 +0300

    was added enum for changing colors of the aim point (video 174)

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -6,6 +6,15 @@
 #include "Components/ActorComponent.h"
 #include "TankAimingComponent.generated.h"
 
+// Enum for aiming state
+UENUM()
+enum class EFiringState : uint8 
+{
+	Reloading, 
+	Aiming, 
+	Locked
+};
+
 // Forward Declaration
 class UTankBarrel;
 class UTankTurret;
@@ -25,10 +34,14 @@ public:
 
 	void AimAt(FVector HitLocation, float LaunchSpeed);
 
-private:
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "State")
+	EFiringState FiringState = EFiringState::Aiming;
 
+private:
 	UTankBarrel* Barrel = nullptr;
 	UTankTurret* Turret = nullptr;
 
 	void MoveBarrelTowards(FVector AimDirection);
+
 };
