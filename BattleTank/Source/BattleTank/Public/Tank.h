4d51c1c15e57b564186e39e3480cd5fbeaf751ea
v_miturev <v_miturev@wargaming.net>
2018-12-07 09:53:34 +0300

    was added health bar logic and ui element which was based on blueprints (video 204)

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -13,10 +13,15 @@ class BATTLETANK_API ATank : public APawn
 public:
 	// Called by the engine when actor damage is dealt
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
+	
+	// Return current health as a percentage of starting health, between 0 and 1
+	UFUNCTION(BlueprintPure, Category = "Health")
+		float GetHealthPercent() const;
+
+private:
 	// Sets default values for this pawn's properties
 	ATank();
 
-private:
 	UPROPERTY(EditDefaultsOnly, Category = "Setup")
 		int32 StartingHealth = 100;
 	UPROPERTY(VisibleAnywhere, Category = "Health")
